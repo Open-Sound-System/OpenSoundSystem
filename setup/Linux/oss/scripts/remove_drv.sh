b930b140f16675342c4951a002310750a7cd1af0
Hannu Savolainen <hannu@opensound.com>
2008-05-21 22:45:38 +0300

    Fixed typo in Linux/remove_drv.sh

--- setup/Linux/oss/scripts/remove_drv.sh
+++ setup/Linux/oss/scripts/remove_drv.sh
@@ -90,7 +90,7 @@ fi
 
 for timeout in 0 1 2 3 4 5 6 7 8 9 10 11
 do
-   if test "`cat /proc/modules|grep ^snd_|sed 's/ .*//'` " == " "
+   if test "`cat /proc/modules|grep ^snd_|sed 's/ .*//'` " = " "
    then
       break
    fi
