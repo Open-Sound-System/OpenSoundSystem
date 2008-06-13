276b9197529dd3d11166056748b1576e353eaf63
Yair K. <devnull@localhost>
2008-06-14 00:22:28 +0300

    FreeBSD changes from audio/oss port
       FreeBSD maintains a ports package for OSS[1], which has accumelated a number of patches for OSS. From what I could gather via CVS history and private e-mail, these patches are backward compatible with previous FreeBSD versions, and (per maintainer) correct. The attached patch includes the following changes from FreeBSD:
    
        1. Use M_WAITOK instead of M_NOWAIT when allocating memory in setup/FreeBSD/oss/build/osscore.c.
        2. Try to unbusy device before detaching in setup/FreeBSD/oss/build/bsdpci.inc and setup/FreeBSD/oss/build/bsdvirtual.inc.
    
    [1] http://www.freebsd.org/cgi/cvsweb.cgi/ports/audio/oss/

--- setup/FreeBSD/oss/build/osscore.c
+++ setup/FreeBSD/oss/build/osscore.c
@@ -264,7 +264,7 @@ oss_contig_malloc (unsigned long buffsize, unsigned long memlimit,
   *phaddr = 0;
 
   tmpbuf =
-    (char *) contigmalloc (buffsize, M_DEVBUF, M_NOWAIT, 0ul, memlimit,
+    (char *) contigmalloc (buffsize, M_DEVBUF, M_WAITOK, 0ul, memlimit,
 			   PAGE_SIZE, 0ul);
   if (tmpbuf == NULL)
     {
