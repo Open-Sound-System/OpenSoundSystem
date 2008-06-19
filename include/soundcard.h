1eefc19b72bf1281ad7680dd4d2c8f8fd66e0210
Hannu Savolainen <hannu@opensound.com>
2008-06-19 13:33:34 +0300

    Changed buildid to v4.1-080619

--- include/soundcard.h
+++ include/soundcard.h
@@ -1732,7 +1732,7 @@ typedef struct oss_sysinfo
   int numcards;			/* Number of sound cards in the system */
   int numaudioengines;		/* Number of audio engines in the system */
   char license[16];		/* For example "GPL" or "CDDL" */
-  int filler[236];		/* For future expansion (set to -1) */
+  int filler[236];		/* For future expansion */
 } oss_sysinfo;
 
 typedef struct oss_mixext
