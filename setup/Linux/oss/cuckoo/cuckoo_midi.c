0a9c1775e0cb144bca58a08f28255d7c8979f83e
Hannu Savolainen <hannu@opensound.com>
2008-07-15 23:07:39 +0300

    Fixed the copyright statement of Cuckoo to be GPL 2.1 compatible

--- setup/Linux/oss/cuckoo/cuckoo_midi.c
+++ setup/Linux/oss/cuckoo/cuckoo_midi.c
@@ -9,9 +9,6 @@
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License.
  *
- * Any modifications that may break backward compatibility are strictly
- * prohibited.
- *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
