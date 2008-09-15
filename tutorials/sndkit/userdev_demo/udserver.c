4f6df0fc89426847b97e896aebabded30feeb70a
Hannu Savolainen <hannu@opensound.com>
2008-09-15 09:33:55 +0300

    Improved comments in userdev_demo/udserver.c

--- tutorials/sndkit/userdev_demo/udserver.c
+++ tutorials/sndkit/userdev_demo/udserver.c
@@ -35,9 +35,17 @@ main(int argc, char *argv[])
 	userdev_create_t crea = {0};
 	char cmd[512];
 
+/*
+ * Sample rate/format we would like to use on server side. The client side
+ * can use whatever they want since OSS will automatically do the required 
+ * conversions.
+ */
 	int rate = 48000;
 	int fmt = AFMT_S16_LE;
 	int channels = 2;
+
+
+
 	int tmp;
 	int fragsize=0;
 
@@ -47,6 +55,13 @@ main(int argc, char *argv[])
 		exit(-1);
 	}
 
+/*
+ * Open the server side device. A new userdev instance (client&server)
+ * will automatically get created when the device is opened. However
+ * creation of the client side will be delayed until USERDEV_CREATE_INSTANCE
+ * gets called.
+ */
+
 printf("SERVER_DEVNAME=%s\n", SERVER_DEVNAME);
 	if ((server_fd = open(SERVER_DEVNAME, O_RDWR, 0))==-1)
 	{
@@ -54,6 +69,9 @@ printf("SERVER_DEVNAME=%s\n", SERVER_DEVNAME);
 		exit(-1);
 	}
 
+/*
+ * Create the client side device.
+ */
 	strcpy(crea.name, "Acme test");
 	crea.flags = USERDEV_F_VMIX_ATTACH;
 	crea.match_method = UD_MATCH_PGID;
@@ -66,6 +84,12 @@ printf("PGID=%d\n", crea.match_key);
 		perror("USERDEV_CREATE_INSTANCE");
 		exit(-1);
 	}
+
+/*
+ * Set up the master side parameters such as sampling rate and sample format.
+ * The server application can select whatever format is best for its
+ * purposes. The client side can select different rate/format if necessary.
+ */
 
 	tmp=0;
 	ioctl(server_fd, SNDCTL_DSP_COOKEDMODE, &tmp); /* Turn off conversions */
@@ -114,6 +138,11 @@ printf("Created instance, devnode=%s\n", crea.devnode);
 
 		exit(0);
 	}
+
+/*
+ * Client side code. Simply execute the command that was given in
+ * argv[1]. However replace all %s's by the client side device node name.
+ */
 
 	sprintf(cmd, "OSS_AUDIODEV=%s", crea.devnode);
 	putenv(cmd);
