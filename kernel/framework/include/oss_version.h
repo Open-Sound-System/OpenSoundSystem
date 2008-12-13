75a04e597c1981a2e2e498238d7bbde7a2991836
Hannu Savolainen <hannu@opensound.com>
2008-12-13 17:33:22 +0200

    ***** Started development of OSS v4.2 ****

--- kernel/framework/include/oss_version.h
+++ kernel/framework/include/oss_version.h
@@ -9,6 +9,6 @@
 #define OSS_LICENSE "" /* Empty means commercial license */
 #endif 
 
-#define OSS_VERSION_ID "4.1"
+#define OSS_VERSION_ID "4.2"
 #define OSS_VERSION_STRING OSS_VERSION_ID " (b " OSS_BUILD_ID "/" OSS_COMPILE_DATE ")"
-#define OSS_INTERNAL_VERSION 0x040099
+#define OSS_INTERNAL_VERSION 0x040199
