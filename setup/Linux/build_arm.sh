acead62b318165907e0c69bc53c0f7f9c8316317
Hannu Savolainen <hannu@opensound.com>
2009-04-08 12:18:51 +0300

    ARM Linux related hacking

--- setup/Linux/build_arm.sh
+++ setup/Linux/build_arm.sh
@@ -4,4 +4,22 @@
 
 . ./.directories
 
+rm -rf prototype
+
+mkdir prototype
+mkdir prototype/etc
+echo "OSSLIBDIR=$OSSLIBDIR" > prototype/etc/oss.conf
+mkdir prototype/usr
+mkdir prototype/usr/bin
+mkdir prototype/usr/sbin
+mkdir -p prototype/$OSSLIBDIR
+mkdir prototype/$OSSLIBDIR/etc
+mkdir prototype/$OSSLIBDIR/conf.tmpl
+mkdir prototype/$OSSLIBDIR/lib
+mkdir prototype/$OSSLIBDIR/modules
+
+cp target/bin/* prototype/usr/bin
+cp target/sbin/* prototype/usr/sbin
+cp target/lib/* prototype/$OSSLIBDIR/lib
+
 exit 0
