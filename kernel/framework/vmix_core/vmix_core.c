c2235e473fd77aff0862eaa223de3a72375f90ff
Hannu Savolainen <hannu@opensound.com>
2009-05-29 16:33:56 +0300

    Fixed src=Fast option of vmix under Linux

--- kernel/framework/vmix_core/vmix_core.c
+++ kernel/framework/vmix_core/vmix_core.c
@@ -369,9 +369,9 @@ create_output_controls (int mixer_dev)
 		
   		memset (ext->enum_present, 0, sizeof (ext->enum_present));
 #ifdef CONFIG_OSS_VMIX_FLOAT
-		ext->enum_present[0] = 0x01; // "Fast" is always present
+		ext->enum_present[0] |= 0x01; // "Fast" is always present
 #endif
-		ext->enum_present[0] = 0x040; // As well as "OFF"
+		ext->enum_present[0] |= 0x040; // As well as "OFF"
 #if CONFIG_OSS_GRC_MAX_QUALITY > 7
 #error CONFIG_OSS_GRC_MAX_QUALITY is out of range
 #endif
